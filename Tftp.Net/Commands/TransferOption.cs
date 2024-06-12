14cb42fb396c7107e8faaad97cf44189cbc2affa
Tony Richards <trichards57@pm.me>
2024-06-12 10:38:58 +0100

    Updated transfer option to immutable record.

--- Tftp.Net/Commands/TransferOption.cs
+++ Tftp.Net/Commands/TransferOption.cs
@@ -3,63 +3,44 @@
 // </copyright>
 
 using System;
-using System.Collections.Generic;
 
 namespace Tftp.Net;
 
 /// <summary>
 /// A single transfer options according to RFC2347.
 /// </summary>
-public sealed class TransferOption : IEquatable<TransferOption>
+public readonly record struct TransferOption
 {
-    internal TransferOption(string name, string value)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TransferOption"/> struct.
+    /// </summary>
+    /// <param name="name">The name of the option.</param>
+    /// <param name="value">The value of the option.</param>
+    /// <param name="isAcknowledged">Indicates if the option has been acknowledged.</param>
+    public TransferOption(string name, string value, bool isAcknowledged = false)
     {
         ArgumentException.ThrowIfNullOrEmpty(name);
         ArgumentNullException.ThrowIfNull(value);
 
         Name = name;
         Value = value;
+        IsAcknowledged = isAcknowledged;
     }
 
     /// <summary>
     /// Gets a value indicating whether the option has been acknowledged by the remote party.
     /// </summary>
-    public bool IsAcknowledged { get; internal set; }
+    public bool IsAcknowledged { get; init; }
 
     /// <summary>
     /// Gets the name of the option.
     /// </summary>
-    public string Name { get; private set; }
+    public string Name { get; init; }
 
     /// <summary>
-    /// Gets or sets the value of the option.
+    /// Gets the value of the option.
     /// </summary>
-    public string Value { get; set; }
-
-    public static bool operator !=(TransferOption left, TransferOption right)
-    {
-        return !(left == right);
-    }
-
-    public static bool operator ==(TransferOption left, TransferOption right)
-    {
-        return EqualityComparer<TransferOption>.Default.Equals(left, right);
-    }
-
-    /// <inheritdoc/>
-    public override bool Equals(object obj)
-    {
-        return Equals(obj as TransferOption);
-    }
-
-    /// <inheritdoc/>
-    public bool Equals(TransferOption other)
-    {
-        return other is not null &&
-               IsAcknowledged == other.IsAcknowledged &&
-               Name == other.Name &&
-               Value == other.Value;
-    }
+    public string Value { get; init; }
 
     /// <inheritdoc/>
     public override int GetHashCode()
